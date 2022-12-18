c03d47e29893eedfbdd6a5d106883281959797b5
urassh <shuto.1011@icloud.com>
2022-12-18 21:31:27 +0900

    topにjQueryとcssアニメーションを導入した

--- geometry.js
+++ geometry.js
@@ -1,7 +1,7 @@
 particlesJS("particles-js",{
 	"particles":{
 		"number":{
-			"value":38,//この数値を変更すると幾何学模様の数が増減できる
+			"value":40,//この数値を変更すると幾何学模様の数が増減できる
 			"density":{
 				"enable":true,
 				"value_area":800
